777e4a73b830541fd1fc66e444571f279f0a17df
Paramesh555 <parameshmethuku@gmail.com>
2024-04-16 21:45:06 +0530

    updated services

--- app/src/main/java/org/example/Services/UserBookingSerice.java
+++ app/src/main/java/org/example/Services/UserBookingSerice.java
@@ -3,10 +3,12 @@ package org.example.Services;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.models.User;
+import org.example.util.UserServiceUtil;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 public class UserBookingSerice {
     private User user;
@@ -16,12 +18,34 @@ public class UserBookingSerice {
 //    it is used in the conversion of data between different representations
 //    example: Object Relational Mapping (ORM):which is the process of mapping objects in an object-oriented programming language (such as Java) to tables in a relational database, and vice versa.
 //Serialization and Deserialization: Object mappers can serialize Java objects into formats like JSON or XML, and deserialize data from these formats back into Java objects.
-    private static final String USERS_PATH="../localDB/users.json";
+    private static final String USERS_PATH="app/src/main/java/org/example/Services/UserBookingSerice.java";
 
-    public UserBookingSerice(User user1) throws IOException{
-        this.user = user1;
+    public UserBookingSerice(User user) throws IOException{
+        this.user = user;
         File users = new File(USERS_PATH);
         userList = objectMapper.readValue(users, new TypeReference<List<User>>(){});
+    }
+
+    public Boolean loginUser(){
+        Optional<User> foundUser = userList.stream().filter(user1 -> {
+            return user1.getName().equalsIgnoreCase(user.getName()) && UserServiceUtil.checkPassword(user.getPassword(), user1.getHashedPassword());
+        }).findFirst();
+        return foundUser.isPresent();
+    }
+
+    public Boolean signUp(User user1){
+        try{
+            userList.add(user1);
+            saveUserListToFile();
+            return Boolean.TRUE;
+        }catch (IOException ex){
+            return Boolean.FALSE;
+        }
+    }
+
+    private void saveUserListToFile() throws IOException {
+        File usersFile = new File(USERS_PATH);
+        objectMapper.writeValue(usersFile, userList);
     }
 
 }
--- app/src/main/java/org/example/models/User.java
+++ app/src/main/java/org/example/models/User.java
@@ -9,4 +9,54 @@ public class User {
     private List<Ticket> ticketsBooked;
     private String UserId;
 
+    public User(String name, String password, String hashedPassword, List<Ticket> ticketsBooked, String userId) {
+        this.name = name;
+        this.password = password;
+        this.hashedPassword = hashedPassword;
+        this.ticketsBooked = ticketsBooked;
+        UserId = userId;
+    }
+
+    public User() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getHashedPassword() {
+        return hashedPassword;
+    }
+
+    public void setHashedPassword(String hashedPassword) {
+        this.hashedPassword = hashedPassword;
+    }
+
+    public List<Ticket> getTicketsBooked() {
+        return ticketsBooked;
+    }
+
+    public void setTicketsBooked(List<Ticket> ticketsBooked) {
+        this.ticketsBooked = ticketsBooked;
+    }
+
+    public String getUserId() {
+        return UserId;
+    }
+
+    public void setUserId(String userId) {
+        UserId = userId;
+    }
 }
